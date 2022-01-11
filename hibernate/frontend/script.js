ab74bd58fa55f582a01b48c311a7ed30d990a16d
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2022-01-11 23:17:56 +0100

    startedJs

---
+++ hibernate/frontend/script.js
@@ -0,0 +1,61 @@
+//requete qui genere la liste des country
+const country = [
+  {
+    name: "France",
+    president: "Macron",
+    regions: [
+      {
+        name: "IDF",
+        temperature: 46,
+      },
+      {
+        name: "Moselle",
+        temperature: -12,
+      },
+      {
+        name: "Nord",
+        temperature: -2,
+      },
+      {
+        name: "Sud",
+        temperature: 33,
+      },
+    ],
+  },
+  {
+    name: "Allemagne",
+    president: "Pas merkel",
+    regions: [
+      {
+        name: "bretzel",
+        temperature: -44,
+      },
+      {
+        name: "Cassoulet",
+        temperature: -4,
+      },
+    ],
+  },
+];
+
+// génération du menu déroulant en fonction des pays renovyés par la requête
+let list = document.getElementById("country-select");
+for (let pays of country) {
+  let option = document.createElement("option");
+  option.value = pays.name;
+  option.text = pays.name;
+  list.add(option);
+}
+
+// refresh de la page en fonction du choix
+list.addEventListener("change", function () {
+  let selectedCountry = country[0]; //fetch en fct de selected-country
+  let regionTableau = document.getElementById("tab-region");
+  for (let region of selectedCountry.regions) {
+    let regionCard = document.createElement("div");
+    regionCard.classList.add("region");
+    regionCard.innerText =
+      "Nom : " + region.name + " Il fait : " + region.temperature + "°C";
+    regionTableau.appendChild(regionCard);
+  }
+});
