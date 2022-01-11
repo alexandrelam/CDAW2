7c4a703752d172c1f6044c56a0fb107cf87bca36
Alexandre Lam <alexandrelam@outlook.com>
2022-01-11 12:04:29 +0100

    added iban verify logicé

--- microservices/backend_iban/server.js
+++ microservices/backend_iban/server.js
@@ -6,8 +6,34 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/verify/iban", (req, res) => {
+  const payload = req.body;
+  let response = { isValid: false };
+  const ibanRegex = new RegExp(
+    /[A-Z]{2}\d{2} ?\d{4} ?\d{4} ?\d{4} ?\d{4} ?[\d]{0,2}/,
+    "g"
+  );
+  if (!payload.iban) {
+    return res.status(400).send({
+      message: "Payload not valid! Should have 'iban' key.",
+    });
+  }
+  if (ibanRegex.test(payload.iban)) {
+    response.isValid = true;
+  }
+  return res.json(response);
 });
 
 app.listen(PORT, HOST);
