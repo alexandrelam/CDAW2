cf508bb32897c73194bd5bae3c8ff3aad39f0067
Alexandre Lam <alexandrelam@outlook.com>
2022-01-12 16:58:33 +0100

    fix backend bancaire transation and account dependancy

--- microservices/backend_bancaire/src/main/java/imt/nord/europe/hibernate/model/TransactionModel.java
+++ microservices/backend_bancaire/src/main/java/imt/nord/europe/hibernate/model/TransactionModel.java
@@ -14,21 +14,16 @@ import javax.persistence.*;
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Table(name = "transaction")
-@JsonIdentityInfo(
-        generator = ObjectIdGenerators.PropertyGenerator.class,
-        property = "transactionId")
 public class TransactionModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long transactionId;
 
-    //@JsonBackReference(value="transaction_as_sender")
     @ManyToOne
     @NonNull
     @JoinColumn(name = "sender", referencedColumnName="account_id")
    AccountModel sender;
 
-    //@JsonBackReference(value="transaction_as_receiver")
     @ManyToOne
     @NonNull
     @JoinColumn(name = "receiver", referencedColumnName="account_id")
