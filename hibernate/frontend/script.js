edabc4e1520ca57d130b1e22ded0cd5a21f140ba
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2022-01-12 22:48:32 +0100

    refresh country and regions

--- hibernate/frontend/script.js
+++ hibernate/frontend/script.js
@@ -1,56 +1,72 @@
 //requete qui genere la liste des country
 let countries;
-fetch("http://localhost:8084/api/v1/country")
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (val) {
-    countries = val;
-    generateSelect();
-  });
+fetchCountries();
+function fetchCountries() {
+  fetch("http://localhost:8084/api/v1/country")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (val) {
+      countries = val;
+      generateSelect();
+    })
+    .then(() => refresh());
+}
 
 // génération du menu déroulant en fonction des pays renovyés par la requête
 function generateSelect() {
   let list = document.getElementById("country-select");
+  list.innerHTML =
+    "<option value='' disabled selected>Choisir un pays</option>";
   for (let pays of countries) {
     let option = document.createElement("option");
-    option.value = pays.name;
+    option.value = pays.countryId;
     option.text = pays.name;
     list.add(option);
   }
-  list.addEventListener("change", refresh);
+  list.addEventListener("change", function (event) {
+    refresh(event.target.value);
+  });
 }
 
 // refresh de la page en fonction du choix
 
-function refresh() {
-  let selectedCountry = countries[0]; //fetch en fct de selected-country : à corriger
-  let regionTableau = document.getElementById("tab-region");
-  regionTableau.innerHTML = "";
-  for (let region of selectedCountry.regions) {
-    let regionCard = document.createElement("div");
-    regionCard.classList.add("region");
-    let bouton = regionCard.appendChild(document.createElement("button"));
-    bouton.classList.add("bouton");
-    bouton.innerText = "-";
-    bouton.addEventListener("click", () => {
-      deleteRegion(region.regionId);
+function refresh(idCountry) {
+  let regions;
+  fetch(`http://localhost:8084/api/v1/country/${idCountry}/region`)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (val) {
+      regions = val;
+    })
+    .then(function () {
+      let regionTableau = document.getElementById("tab-region");
+      regionTableau.innerHTML = "";
+      for (let region of regions) {
+        let regionCard = document.createElement("div");
+        regionCard.classList.add("region");
+        let bouton = regionCard.appendChild(document.createElement("button"));
+        bouton.classList.add("bouton");
+        bouton.innerText = "-";
+        bouton.addEventListener("click", () => {
+          deleteRegion(region.regionId);
+        });
+        let nom = document.createElement("span");
+        let temperature = document.createElement("span");
+        nom.innerText = "Nom : " + region.name;
+        temperature.innerText = " Il fait : " + region.temperature + "°C";
+        regionCard.appendChild(nom);
+        regionCard.appendChild(temperature);
+        regionTableau.appendChild(regionCard);
+      }
     });
-    let nom = document.createElement("span");
-    let temperature = document.createElement("span");
-    nom.innerText = "Nom : " + region.name;
-    temperature.innerText = " Il fait : " + region.temperature + "°C";
-    regionCard.appendChild(nom);
-    regionCard.appendChild(temperature);
-    regionTableau.appendChild(regionCard);
-  }
 }
-
 function deleteRegion(id) {
   fetch(`http://localhost:8084/api/v1/region/${id}`, {
     method: "DELETE",
   })
     .then((res) => res.text())
     .then((res) => console.log(res))
-    .then(() => refresh()); // à corriger
+    .then(() => fetchCountries());
 }
