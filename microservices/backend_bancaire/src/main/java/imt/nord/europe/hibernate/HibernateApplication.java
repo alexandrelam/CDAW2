7fc11221bb84481728ea72d9e2e5d6767b3a7db8
Alexandre Lam <alexandrelam@outlook.com>
2022-01-12 17:02:57 +0100

    better factory

--- microservices/backend_bancaire/src/main/java/imt/nord/europe/hibernate/HibernateApplication.java
+++ microservices/backend_bancaire/src/main/java/imt/nord/europe/hibernate/HibernateApplication.java
@@ -2,6 +2,7 @@ package imt.nord.europe.hibernate;
 
 import imt.nord.europe.hibernate.model.*;
 import imt.nord.europe.hibernate.repository.AccountRepository;
+import imt.nord.europe.hibernate.repository.TransactionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,11 +18,20 @@ public class HibernateApplication {
     }
 
     @Bean
-    public CommandLineRunner factory(AccountRepository accountRepository) {
+    public CommandLineRunner factory(AccountRepository accountRepository, TransactionRepository transactionRepository) {
         return (args) -> {
-            accountRepository.save(new AccountModel("FR7620041010169876543210921", 0));
-            accountRepository.save(new AccountModel("FR7630003035409876543210925", 100));
-            accountRepository.save(new AccountModel("FR7630004028379876543210943", 10));
+            AccountModel accountA = new AccountModel("FR7620041010169876543210921", 0);
+            AccountModel accountB = new AccountModel("FR7630003035409876543210925", 100);
+            AccountModel accountC = new AccountModel("FR7630004028379876543210943", 100);
+
+            accountRepository.save(accountA);
+            accountRepository.save(accountB);
+            accountRepository.save(accountC);
+
+            transactionRepository.save(new TransactionModel(accountA, accountB, 2300));
+            transactionRepository.save(new TransactionModel(accountB, accountA, 1000));
+            transactionRepository.save(new TransactionModel(accountC, accountA, 1000));
+
         };
     }
 }
