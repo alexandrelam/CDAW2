f6a88bb1f267a93df044bb67b60d4907d1c87bb9
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2022-01-19 23:58:48 +0100

    added accountcard/list

--- microservices/frontend_bancaire/components/Test.js
+++ microservices/frontend_bancaire/components/Test.js
@@ -1,15 +1,6 @@
-import React, { useState, useEffect } from "react";
 import { centsToPrice, makeTransaction } from "../utils/helper";
 
 export default function Test() {
-  useEffect(() => {
-    fetch("http://localhost:8084/api/v1/account")
-      .then((res) => res.json())
-      .then((val) => setresponse(val));
-  }, []);
-
-  const [response, setresponse] = useState([]);
-
   const handleTransaction = () => {
     makeTransaction(
       "FR7620041010169876543210921",
