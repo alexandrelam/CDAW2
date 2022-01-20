ae9be527cc0690d3341b27d61deab06d75724134
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2022-01-20 23:19:58 +0100

    button for transaction + transaction form

--- microservices/frontend_bancaire/components/Test.js
+++ microservices/frontend_bancaire/components/Test.js
@@ -1,18 +1,6 @@
 import { makeTransaction } from "../utils/helper";
 
 export default function Test() {
-  const handleTransaction = () => {
-    makeTransaction(
-      "FR7620041010169876543210921",
-      "FR7630003035409876543210925",
-      2000
-    )
-      .then((res) => {
-        console.log(res);
-      })
-      .catch(console.log);
-  };
-
   return (
     <>
       <button onClick={handleTransaction}>make transaction</button>
